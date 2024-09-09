948e045a7d0a07b9b61368fd6c0d23016813c0f4
Lindsey Howard <codelikeagirl91@gmail.com>
2024-09-08 21:05:42 -0500

    add tweet btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ImQuotesLeft, ImQuotesRight } from 'react-icons/im';
+import { CiTwitter } from 'react-icons/ci';
 import './App.css';
 
 function App() {
@@ -27,8 +28,11 @@ function App() {
 	}, []);
 	return (
 		<div className='col-md-6 p-5 p-md-5 d-flex flex-column align-items-center justify-content-center gap-5 w-80 vh-100 font-sans block_content text-center'>
-			<div className='container py-5 h-100 quote'>
-				<div className='row d-flex justify-content-center align-items-center h-100 mb-0 blockquote'>
+			<div className='container py-5 h-100 quote' id='quote-box'>
+				<div
+					className='row d-flex justify-content-center align-items-center h-100 mb-0 blockquote'
+					id='text'
+				>
 					{randomQuote && (
 						<article className='shadow-md w-100 h-auto rounded-xl p-4 text-dark '>
 							<p className='d-flex align-items-start flex-column flex-md-row gap-2  p-2 rounded-md shadow-md font-weight-bold mb-3 blockstyle note text-secondary '>
@@ -37,15 +41,27 @@ function App() {
 								{randomQuote.quote}
 								<ImQuotesRight />
 							</p>
-							<p className='blockquote-footer'> {randomQuote.author}</p>
+							<p className='blockquote-footer' id='author'>
+								{' '}
+								{randomQuote.author}
+							</p>
 						</article>
 					)}
 					<button
 						onClick={generateRandomQuote}
 						className='btn btn-primary border border-black p-2 rounded-lg hover:text-black hover:bg-white transition duration-75'
+						id='new-quote'
 					>
 						Generate Quote
 					</button>
+					<a
+						href=''
+						className='btn btn-primary border border-black p-2 rounded-lg hover:text-black hover:bg-white transition duration-75'
+						id='tweet-quote'
+						target='_blank'
+					>
+						<CiTwitter />
+					</a>
 				</div>
 			</div>
 		</div>
